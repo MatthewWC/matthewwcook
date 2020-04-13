c4acee9fd246e3f46f676b14ed91cacb73b67d95
Matthew Cook <matthew.w.cook94@gmail.com>
2020-04-13 10:04:01 -0400

    init nav bar

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,7 +7,7 @@ function Home() {
   return(
     <Grid container justify='center'>
       <Typography>
-        Hey, everyone!. My name is Matthew Cook, and this is my personal page for showcasing what I've been up to these last few years
+        Hey, everyone! My name is Matthew Cook, and this is my personal page for showcasing what I've been up to these last few years
         and a place for employeers to stop by and check out some of my skills and the work that I've done. Dive in and learn a little 
         about who I am, and what I can do.
       </Typography>
